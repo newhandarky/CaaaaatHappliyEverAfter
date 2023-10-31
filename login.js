51c339dc446bfea79cab2807fdb335e40e0e214d
Leo <leo80103@gmail.com>
2023-10-31 20:11:53 +0800

    新增會員登入功能

---
+++ login.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+//抓到 Dom 元素
+const host = "https://catroomdb.onrender.com";
+
+const loginForm = document.querySelector("form");
+const loginBtn = document.querySelector(".email");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let email = e.target[0].value;
+  let password = e.target[1].value;
+  console.log(email);
+  console.log(password);
+
+  // 登入 API
+
+  axios
+    .post(`${host}/login`, {
+      email,
+      password,
+    })
+    .then((res) => {
+      let { user, accessToken } = res.data;
+      //獲得的用戶資料存入 localStorage
+      localStorage.setItem("myUser", JSON.stringify(user));
+      localStorage.setItem("myToken", JSON.stringify(accessToken));
+
+      console.log(JSON.parse(localStorage.getItem("myUser")));
+      console.log(JSON.parse(localStorage.getItem("myToken")));
+    })
+    .catch((err) => {
+      //錯誤提示
+      console.log(err.response.data);
+      alert(err.response.data);
+    });
+});
