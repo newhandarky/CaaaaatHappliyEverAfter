6f78fd1c081de042e9c51753278bc155c69f3574
Leo <leo80103@gmail.com>
2023-11-02 15:01:43 +0800

    我的貓咪頁面新增

--- navbar.js
+++ navbar.js
@@ -2,6 +2,7 @@ import { isLogin } from "./isLogin";
 
 //進入會員功能前 判斷是不是已登入
 const memberData = document.getElementById("memberData");
+const catData = document.getElementById("catData");
 
 memberData.addEventListener("click", (e) => {
   e.preventDefault();
@@ -12,4 +13,15 @@ memberData.addEventListener("click", (e) => {
   // _url 是後端主機位置
   // memberHerf 試驗成功登入後 要前往的連結位置 形式像是 "../member.html"
   isLogin(memberHerf);
+});
+
+catData.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const catHerf = catData.getAttribute("href");
+
+  // isLogin 是自訂一的函數 判斷登入狀態 (只能用在 a 標籤的點擊上)
+  // _url 是後端主機位置
+  // memberHerf 試驗成功登入後 要前往的連結位置 形式像是 "../member.html"
+  isLogin(catHerf);
 });
