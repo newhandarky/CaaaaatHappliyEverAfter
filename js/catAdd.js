665da4917bb74d40228abbc6e6cff7a2fec124ea
Leo <leo80103@gmail.com>
2023-11-05 14:26:35 +0800

    新增貓咪功能架設

---
+++ js/catAdd.js
@@ -0,0 +1,122 @@
+import axios from "axios";
+import { _url } from "./config";
+import { isLogin } from "./isLogin";
+
+//取得所需要的資療及DOM元素
+const userTokenAndData = JSON.parse(localStorage.getItem("userTokenAndData"));
+const { accessToken, user } = userTokenAndData;
+const addCatFrom = document.getElementById("addCatFrom");
+const cancelAdd = document.getElementById("cancelAdd");
+const myCatFile = document.getElementById("myCatFile");
+
+// 上傳圖片需要轉存為 Base64 數據宣告
+const imageData = {};
+
+// // 新增貓咪資料 function
+function catAddData(catData) {
+  axios
+    .post(`${_url}/cats`, catData, {
+      headers: {
+        authorization: `Bearer ${accessToken}`,
+      },
+    })
+    .then((res) => {
+      console.log(res);
+      alert("新增貓咪資料成功");
+      window.location.href = "./catData.html";
+    })
+    .catch((err) => {
+      //錯誤提示
+      console.log(err);
+      alert(
+        `新增貓咪資料失敗 請重新登入後嘗試 /n錯誤提示：${err.response.data}`
+      );
+      window.location.href = "./login.html";
+    });
+}
+
+// //按下完成新增
+addCatFrom.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //先判斷是否為登入狀態
+  // isLoginStay 是自訂一的函數 判斷登入狀態
+  isLogin();
+
+  //抓取表單資料 除了圖片
+
+  const catNameDom = document.getElementById("catName");
+  const catMaleDom = document.getElementById("catMale");
+  const catFemaleDom = document.getElementById("catFemale");
+  const catOtherDom = document.getElementById("catOther");
+  const catBirthdayDom = document.getElementById("catBirthday");
+  const catBreedsDom = document.getElementById("catBreeds");
+  const catColorsDom = document.getElementById("catColors");
+  const catWeightDom = document.getElementById("catWeight");
+
+  //判斷哪個性別被選取
+  let gender = () =>
+    // 使用三元条件运算符来判断哪个性别被选取，并返回相应的值
+    catMaleDom.checked
+      ? catMaleDom.value
+      : catFemaleDom.checked
+      ? catFemaleDom.value
+      : catOtherDom.checked
+      ? catOtherDom.value
+      : "Not Specified";
+
+  console.dir(catMaleDom);
+
+  let catData = {
+    catName: catNameDom.value,
+    catBreeds: catBirthdayDom.value,
+    colors: catColorsDom.value,
+    gender: gender(),
+    birthday: catBreedsDom.value,
+    weight: catWeightDom.value,
+    catPhoto: imageData.base64Image || "https://i.imgur.com/slCGtuf.png",
+    userId: user.id,
+  };
+  console.log(catData);
+
+  // // 使用新增貓咪函數來新增貓咪會員資料
+  catAddData(catData);
+});
+
+// 按下取消新增時 偵測是否為登入狀態
+cancelAdd.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const cancelAddHerf = cancelAdd.getAttribute("href");
+
+  // isLoginToHref 是自訂一的函數 判斷登入狀態 需要帶入前往的網址頁面路徑
+  isLogin(cancelAddHerf);
+});
+
+//上傳圖片
+
+myCatFile.addEventListener("change", (event) => {
+  // 抓到選擇的圖片檔案描述
+  const selectedFile = event.target.files[0];
+
+  // 轉換圖片格式為 Base64
+  const reader = new FileReader();
+
+  reader.onload = (e) => {
+    const base64Data = e.target.result;
+
+    // 将图像数据编码为 Base64 字符串
+
+    imageData.base64Image = base64Data;
+    imageData.fileName = selectedFile.name;
+
+    // 将图像数据显示在<img>元素中
+    const catPhotoDom = document.getElementById("catPhoto");
+    catPhotoDom.src = imageData.base64Image;
+
+    // 在这里可以将 imageData 发送到后端，更新用户的头像等操作
+  };
+
+  reader.readAsDataURL(selectedFile);
+  console.log(imageData);
+});
--- js/catData.js
+++ js/catData.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { _url } from "./config";
+import { isLogin } from "./isLogin";
 
 //取得所需要的資療及DOM元素
 const userTokenAndData = JSON.parse(localStorage.getItem("userTokenAndData"));
 const { accessToken, user } = userTokenAndData;
 const catEdit = document.getElementById("catEdit");
 const catDelete = document.getElementById("catDelete");
+const catAdd = document.getElementById("catAdd");
 
 //創建 呈現貓咪資料的 HTML DOM 元素
 
@@ -51,11 +53,11 @@ function lodingCat() {
           <li id="catBirthday_${id}" class="catBirthday">生日：${birthday}</li>
           <li id="catBreeds_${id}" class="catBreeds">品種：${catBreeds}</li>
           <li id="catColors_${id}" class="catColors">花色：${colors}</li>
-          <li id="catWeight_${id}" class="catWeight">體重：${weight}</li>
+          <li id="catWeight_${id}" class="catWeight">體重：${weight} Kg</li>
         </ul>
         <img id="catPhoto_${id}" src="${catPhoto}" alt="貓咪照片" />
         <br />
-        <button id="catEdit_${id}" class="catEdit">編輯資料</button>
+        <button id="catEdit_${id}" data-catId="${id}" class="catEdit">編輯資料</button>
         <button id="catDelete_${id}" class="catDelete">刪除資料</button>
       </div>
       `);
@@ -94,3 +96,14 @@ function lodingCat() {
 }
 
 lodingCat();
+
+//當按下新增貓咪檢查是否為登入狀態
+
+catAdd.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const catAddHerf = catAdd.getAttribute("href");
+  console.log(catAddHerf);
+
+  isLogin(catAddHerf);
+});
