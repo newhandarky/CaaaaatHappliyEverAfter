a1ed9f6ecc8bba77f13d4e73a8fd40500c31c787
Leo <leo80103@gmail.com>
2023-11-07 11:41:13 +0800

    訂單紀錄 結構架設

--- js/navbar.js
+++ js/navbar.js
@@ -3,6 +3,7 @@ import { isLogin } from "./isLogin";
 //進入會員功能前 判斷是不是已登入
 const memberData = document.getElementById("memberData");
 const catData = document.getElementById("catData");
+const bookingHistory = document.getElementById("bookingHistory");
 
 memberData.addEventListener("click", (e) => {
   e.preventDefault();
@@ -21,4 +22,14 @@ catData.addEventListener("click", (e) => {
 
   // isLogin 是自訂一的函數 判斷登入狀態 可以在確認後前往的網址頁面路徑
   isLogin(catHerf);
+});
+
+//進入訂房紀錄前 判斷是不是已登入
+bookingHistory.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const bookingHistoryHerf = bookingHistory.getAttribute("href");
+
+  // isLogin 是自訂一的函數 判斷登入狀態 可以在確認後前往的網址頁面路徑
+  isLogin(bookingHistoryHerf);
 });
