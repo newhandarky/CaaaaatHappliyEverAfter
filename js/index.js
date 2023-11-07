646b8f0a325eacf558650cb90fe688a0346c2288
Leo <leo80103@gmail.com>
2023-11-07 14:52:41 +0800

    index.js 先初始化 之後會在架構

--- js/index.js
+++ js/index.js
@@ -1,8 +1,1 @@
-// import axios from "axios";
-
-axios.get("https://catroomdb.onrender.com/users")
-    .then(function (res) {
-        console.log(res.data);
-    }).catch(function (err) {
-        console.log(err.response);
-    })
+console.log("歡迎來到首頁");
