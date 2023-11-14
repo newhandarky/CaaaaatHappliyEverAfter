929967f3dabe931e60599d031b5b1e30fe66d7ca
Leo <leo80103@gmail.com>
2023-11-14 18:12:19 +0800

    首頁切版開始

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 console.log("歡迎來到首頁");
+import flatpickr from "flatpickr";
+
+const catRoomInput_checkIn = document.getElementById("catRoomInput_checkIn");
+const catRoomInput_checkOut = document.getElementById("catRoomInput_checkOut");
+
+const checkInInput = document.getElementById("checkIn");
+const checkOutInput = document.getElementById("checkOut");
+
+//flatpickr 套件使用
+flatpickr("#checkIn", {
+  enableTime: false,
+  dateFormat: "Y-m-d",
+});
+
+catRoomInput_checkIn.addEventListener("click", (e) => {
+  e.preventDefault();
+  checkInInput.focus();
+});
+//flatpickr 套件使用
+flatpickr("#checkOut", {
+  enableTime: false,
+  dateFormat: "Y-m-d",
+});
+
+catRoomInput_checkOut.addEventListener("click", (e) => {
+  e.preventDefault();
+  checkOutInput.focus();
+});
