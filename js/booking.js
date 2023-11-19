8fc81ac9cbf161c47a0a8a9ee90ed48f9d497736
Flora Kuo In The House <tingyu1112@gmail.com>
2023-11-19 17:03:56 +0800

    新增經典房與豪華房訂房功能

--- js/booking.js
+++ js/booking.js
@@ -4,7 +4,6 @@ import { _url } from "./config";
 import { isLogin } from "./isLogin";
 
 
-//bookingClassicRoom.html
 let bookingNow = document.querySelector(".bookingNow");
 // console.log(bookingNow);
 bookingNow.addEventListener("click", function(e){
@@ -14,41 +13,123 @@ bookingNow.addEventListener("click", function(e){
 });
 
 
-//bookingPetiteRoom.html
+//bookingdelicateRoom.html
 //DOM
-let petiteRoomFacility = document.querySelector(".petiteRoomFacility");
-let petiteRoomDimension = document.querySelector(".petiteRoomDimension");
-let petiteRoomCatNum = document.querySelector(".petiteRoomCatNum");
-let petiteRoomPrice = document.querySelector(".petiteRoomPrice");
+let delicateRoomFacility = document.querySelector(".delicateRoomFacility");
+let delicateRoomDimension = document.querySelector(".delicateRoomDimension");
+let delicateRoomCatNum = document.querySelector(".delicateRoomCatNum");
+let delicateRoomPrice = document.querySelector(".delicateRoomPrice");
 //size
-axios.get('https://catroomdb.onrender.com/rooms/52').then(function(response){
-    let petiteRoomDimensionClaim = response.data.size;
-    let str = ``;
-    petiteRoomDimensionClaim.forEach(function(item){
-        let content = `${item}cm `;b
+axios.get(`${_url}/rooms/52`).then(function(response){
+    let delicateRoomDimensionClaim = response.data.size;
+    let str = `尺寸：`;
+    delicateRoomDimensionClaim.forEach(function(item){
+        let content = `${item}cm `;
         str += content;
-        petiteRoomDimension.innerHTML=str
+        delicateRoomDimension.innerHTML=str
     })
 })
 //設備
-axios.get('https://catroomdb.onrender.com/rooms/52').then(function(response){
-    let petiteRoomFacilityList = response.data.facility;
+axios.get(`${_url}/rooms/52`).then(function(response){
+    let delicateRoomFacilityList = response.data.facility;
     let str = ``
-    petiteRoomFacilityList.forEach(function(item){
+    delicateRoomFacilityList.forEach(function(item){
         let content = `<li>${item}</li>`
         str += content;
     });
-    petiteRoomFacility.innerHTML= str;
+    delicateRoomFacility.innerHTML= str;
 
 });
 //cat number
-axios.get('https://catroomdb.onrender.com/rooms/52').then(function(response){
-    let petiteRoomCat = response.data.quantity;
-    petiteRoomCatNum.innerHTML = petiteRoomCat
+axios.get(`${_url}/rooms/52`).then(function(response){
+    let delicateRoomCat = response.data.quantity;
+    delicateRoomCatNum.innerHTML = delicateRoomCat
 })
 //price
-axios.get('https://catroomdb.onrender.com/rooms/52').then(function(response){
-    let petiteRoomMoney = response.data.price;
-    petiteRoomPrice.innerHTML = petiteRoomMoney
-})
+axios.get(`${_url}/rooms/52`).then(function(response){
+    let delicateRoomMoney = response.data.price;
+    delicateRoomPrice.innerHTML = delicateRoomMoney
+});
 
+//bookingClassicRoom.html
+//DOM
+let classicRoomDimension = document.querySelector('.classicRoomDimension');
+let classicRoomFacility = document.querySelector('.classicRoomFacility');
+let classicRoomCatNum = document.querySelector('.classicRoomCatNum');
+let classicRoomPrice = document.querySelector('.classicRoomPrice');
+
+//size
+axios.get(`${_url}/rooms/51`).then(response =>{
+    let classicRoomDimensionClaim = response.data.size
+    let str = `尺寸：`;
+    classicRoomDimensionClaim.forEach(function(item){
+        let content = `${item}cm`;
+        str += content;
+    })
+    classicRoomDimension.innerHTML=str
+});
+
+//設備
+axios.get(`${_url}/rooms/51`).then(function(response){
+    let classicRoomFacilityList = response.data.facility;
+    let str = ``
+    classicRoomFacilityList.forEach(function(item){
+        let content = `<li>${item}</li>`
+        str += content;
+    });
+    classicRoomFacility.innerHTML= str;
+
+});
+
+//cat number
+axios.get(`${_url}/rooms/51`).then(function(response){
+    let classicRoomCat = response.data.quantity;
+    classicRoomCatNum.innerHTML = classicRoomCat
+})
+//price
+axios.get(`${_url}/rooms/51`).then(function(response){
+    let classicRoomMoney = response.data.price;
+    classicRoomPrice.innerHTML = classicRoomMoney
+});
+
+
+//bookingLuxRoom.html
+//DOM
+let luxRoomDimension= document.querySelector('.luxRoomDimension');
+let luxRoomFacility = document.querySelector('.luxRoomFacility');
+let luxRoomCatNum = document.querySelector('.luxRoomCatNum');
+let luxRoomPrice = document.querySelector('.luxRoomPrice');
+
+//size
+axios.get(`${_url}/rooms/53`).then(response =>{
+    let luxRoomDimensionClaim = response.data.size
+    let str = `尺寸：`;
+    luxRoomDimensionClaim.forEach(function(item){
+        let content = `${item}cm`;
+        str += content;
+    })
+    luxRoomDimension.innerHTML=str
+});
+
+//設備
+axios.get(`${_url}/rooms/53`).then(function(response){
+    let luxRoomFacilityListClaim = response.data.facility;
+    let str = ``
+    luxRoomFacilityListClaim.forEach(function(item){
+        let content = `<li>${item}</li>`
+        str += content;
+    });
+    luxRoomFacility.innerHTML= str;
+
+});
+
+//cat number
+axios.get(`${_url}/rooms/53`).then(function(response){
+    let luxRoomCatNumClaim = response.data.quantity;
+    luxRoomCatNum.innerHTML = luxRoomCatNumClaim
+})
+//price
+axios.get(`${_url}/rooms/53`).then(function(response){
+    let luxRoomPriceClaim = response.data.price;
+    luxRoomPrice.innerHTML = luxRoomPriceClaim 
+});
