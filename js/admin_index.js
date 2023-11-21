d5154ae31759e9750c97e167241a2ad683fc0ede
michaelhsia <michaelhsia8716@gmail.com>
2023-11-21 16:15:38 +0800

    完成後台登入功能

--- js/admin_index.js
+++ js/admin_index.js
@@ -1,8 +1,29 @@
+// 若沒有登入直接跳到登入頁面
+if (localStorage.length === 0) {
+  backToLogin();
+}
+
+function backToLogin() {
+  location.href = "admin_login.html";
+}
+
 /*------------------------------------*\
     儲存所需資料
 \*------------------------------------*/
-localStorage.setItem("userRole", JSON.parse(localStorage.getItem("userTokenAndData")).user.role);
-localStorage.setItem("userName", JSON.parse(localStorage.getItem("userTokenAndData")).user.name);
-localStorage.setItem("userId", JSON.parse(localStorage.getItem("userTokenAndData")).user.id);
-localStorage.setItem("userLoginToken", JSON.parse(localStorage.getItem("userTokenAndData")).accessToken);
+localStorage.setItem(
+  "userRole",
+  JSON.parse(localStorage.getItem("userTokenAndData")).user.role
+);
+localStorage.setItem(
+  "userName",
+  JSON.parse(localStorage.getItem("userTokenAndData")).user.name
+);
+localStorage.setItem(
+  "userId",
+  JSON.parse(localStorage.getItem("userTokenAndData")).user.id
+);
+localStorage.setItem(
+  "userLoginToken",
+  JSON.parse(localStorage.getItem("userTokenAndData")).accessToken
+);
 localStorage.removeItem("userTokenAndData");
