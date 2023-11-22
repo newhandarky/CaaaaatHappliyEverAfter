08505c4a7fd3ae31c68ba0ae9b3d48392d4070ef
Leo <leo80103@gmail.com>
2023-11-22 17:17:40 +0800

    串接後端訂單呈現資料成功

--- js/bookingHistory.js
+++ js/bookingHistory.js
@@ -28,23 +28,108 @@ function lodingBookin() {
 
       allBookingData.forEach((element, index) => {
         //抽出所需要的資料
-        const { bookingDate, checkIn, checkOut, state, price, room, id } =
-          element;
+        const {
+          bookingDate,
+          checkIn,
+          checkOut,
+          quantity,
+          state,
+          price,
+          room,
+          id,
+        } = element;
         const { name } = room;
-
         // console.log(bookingDate, checkIn, checkOut, state, price, name);
 
         //抓到 DOM 並呈現資料
         const bookingInfo = document.getElementById("bookingInfo");
-        bookingInfo.innerHTML = `<h3 id="checkIn" class="checkDate">${checkIn}</h3>
-        <h3>→</h3>
-        <h3 id="checkOut_${index}" class="checkDate">${checkOut}</h3>
-        <h3 id="roomType_${index}" class="roomType">${name}</h3>
-        <h5 id="bookingId_${index}" class="bookingId">訂單編號 ${id}</h5>
-        <h1 id="price_${index}" class="price">${price}</h1>
-        <h5 id="bookingDate_${index}" class="bookingDate">下單日期 ${bookingDate}</h5>
-        <a id="editBooking_${index}" data-Index="${index}" data-catId="${id}" class="editBooking" href="./editBooking.html">修改訂單</a>
-        <button id="cancelBooking_${index}" data-Index="${index}" data-catId="${id}" class="cancelBooking">取消訂單</button>`;
+        bookingInfo.innerHTML += ` <div class="catBarContainer">
+        <div class="catEars">
+          <div class="catEar">
+            <img src="../assets/images/catEar02.svg" alt="catEar02" />
+          </div>
+          <div class="catEar">
+            <img src="../assets/images/catEar02.svg" alt="catEar02" />
+          </div>
+        </div>
+        <div class="catBarBorder mx-3">
+          <div class="catBarInfo d-flex flex-column">
+            <div class="justify-content-center align-items-center">
+              <div class="d-flex flex-sm-row flex-column mb-5">
+                <h3 id="checkIn_${index}" class="d-inline-block">
+                  ${checkIn}
+                </h3>
+                <div class="d-flex justify-content-center mx-3">
+                  <img
+                    class="img-fluid d-sm-block d-none"
+                    src="../assets/icons/arrow.svg"
+                    alt="arrow"
+                  />
+                  <img
+                    class="img-fluid d-sm-none d-block"
+                    src="../assets/icons/arrowDown.svg"
+                    alt="arrow"
+                  />
+                </div>
+
+                <h3 id="checkOut_${index}" class="d-inline-block">
+                  ${checkOut}
+                </h3>
+              </div>
+              <div class="d-flex flex-sm-row flex-column">
+                <div class="d-flex flex-column">
+                  <div class="d-flex">
+                    <h3 id="roomType_${index}" class="roomType">
+                      ${name}
+                    </h3>
+
+                    <h4
+                      id="catsQuantity_${index}"
+                      class="mt-1 catsQuantity"
+                    >
+                      ${quantity}隻貓
+                    </h4>
+                  </div>
+                  <div class="d-flex">
+                    <h5 class="text-center">訂單編號</h5>
+                    <h5
+                      id="bookingId_${index}"
+                      class="text-center bookingId"
+                    >
+                      ${id}
+                    </h5>
+                  </div>
+                  <div class="d-flex">
+                    <h6 class="orderDate">下單日期</h6>
+                    <h6
+                      id="bookingId_${index}"
+                      class="orderDate bookingId"
+                    >
+                      ${bookingDate}
+                    </h6>
+                  </div>
+                </div>
+                <div class="d-flex flex-grow-5 ms-sm-5 ms-0">
+                  <h1 id="price_${index}" class="price">${price}NT</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="orderBtns">
+            <div id="btntype" class="primaryFill-btn-primary">
+              <button
+                id="cancelBooking_${index}"
+                data-catId="${id}"
+                class="orderBtn"
+              >
+                取消訂單
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br>
+      `;
       });
     })
     .catch((err) => {
