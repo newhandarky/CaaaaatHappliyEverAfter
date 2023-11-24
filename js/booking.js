c8698403633fa0fc1d7f5c1b000bb74a7f8371c1
Flora Kuo In The House <tingyu1112@gmail.com>
2023-11-24 17:54:57 +0800

    完成首頁快速訂房功能

--- js/booking.js
+++ js/booking.js
@@ -2,6 +2,140 @@
 import axios from "axios";
 import { _url } from "./config";
 import { isLogin } from "./isLogin";
+import Swal from 'sweetalert2';
+
+console.log("suc")
+
+//首頁的快速預約房型
+//DOM
+const searchBarBtn = document.querySelector('.searchBarBtn');
+const checkIn = document.querySelector('#checkIn');
+const checkOut = document.querySelector('#checkOut');
+const roomType = document.querySelector('#roomType');
+console.log(roomType.value )
+
+searchBarBtn.addEventListener("click", function(e){
+        //console.log("請填寫入住日期、退房日期與選擇房型");
+        if(checkOut.value == "" ||checkIn.value == "" || roomType.value == "請選擇" ){
+            // alert("請填寫入住日期、退房日期與選擇房型")
+            // Swal.fire(`您尚未選擇入住或退房時間或選擇房型`)
+            alert("您尚未選擇入住或退房時間或選擇房型")
+            console.log("請填寫入住日期、退房日期與選擇房型");
+        }
+
+        else if(checkOut.value <= checkIn.value){
+            alert("退房日期需晚於入住日期")
+             console.log("退房日期需晚於入住日期");
+             checkOut.value = "";
+             checkIn.value = "";
+             return
+        }
+
+        else if (roomType.value == "經典房"){
+            console.log("經典房");
+            axios.get(`${_url}/roomStates?date_gte=${checkIn.value}&date_lte=${checkOut.value}&date_ne=${checkOut.value}`).then(function(response){
+                let data = response.data;
+                console.log(data)
+                data.forEach(function(item){
+                 if(item.availableCount.classic <= 0){
+                    alert(`${item.date}已無空房，請重新選擇`);
+                    checkOut.value = "";
+                    checkIn.value = "";
+                    return
+                    
+                 }else{
+                    indexBooking()
+                 }
+        })})
+        }
+
+        else if (roomType.value == "精緻房"){
+            console.log("精緻房");
+            axios.get(`${_url}/roomStates?date_gte=${checkIn.value}&date_lte=${checkOut.value}&date_ne=${checkOut.value}`).then(function(response){
+                let data = response.data;
+                console.log(data)
+                data.forEach(function(item){
+                 if(item.availableCount.delicate <= 0){
+                    alert(`${item.date}已無空房，請重新選擇`);
+                    checkOut.value = "";
+                    checkIn.value = "";
+                    return
+                    
+                 }else{
+                    indexBooking()
+                 }
+        })});
+        }
+
+        else if (roomType.value == "豪華房"){
+            console.log("豪華房");
+            axios.get(`${_url}/roomStates?date_gte=${checkIn.value}&date_lte=${checkOut.value}&date_ne=${checkOut.value}`).then(function(response){
+                let data = response.data;
+                console.log(data)
+                data.forEach(function(item){
+                 if(item.availableCount.luxury <= 0){
+                    alert(`${item.date}已無空房，請重新選擇`);
+                    checkOut.value = "";
+                    checkIn.value = "";
+                    return
+                    
+                 }else{
+                    indexBooking()
+                 }
+        })});
+        }
+        
+    
+});
+
+function indexBooking(){
+    let indexBookingArr = [];
+    let indexBooking = {};
+    indexBooking['checkIn'] = checkIn.value;
+    indexBooking['checkOut'] = checkOut.value;
+    indexBooking['roomType'] = roomType.value;
+    console.log(indexBooking);
+    indexBookingArr.push(indexBooking);
+    let indexBookingString = JSON.stringify(indexBookingArr);
+    console.log(indexBookingString);
+    sessionStorage.setItem('indexBooking', indexBookingString);
+    
+    if(roomType.value == "經典房"){
+        window.location.href = "./bookingClassicProcess_1.html"
+    };
+    if(roomType.value == "精緻房"){
+        window.location.href = "./bookingDelicateProcess_1.html"
+    };
+    if(roomType.value == "豪華房"){
+        window.location.href = "./bookingLuxProcess_1.html"
+    }
+    
+};
+
+
+//找到今天的日期
+let date = new Date();
+let currentYear = date.getFullYear();
+console.log(currentYear);
+console.log(date.getDate());
+console.log(date.getMonth()+1);
+let currentDate = `${currentYear}-${date.getMonth()+1}-${date.getDate()}`;
+console.log(currentDate);
+
+    //flatpickr
+    flatpickr("#checkIn", {
+        enableTime: false,
+        dateFormat: "Y-m-d",
+        minDate: currentDate,
+        maxDate: "2024-02-29"
+      });
+    
+      flatpickr("#checkOut", {
+        enableTime: false,
+        dateFormat: "Y-m-d",
+        minDate: currentDate,
+        maxDate: "2024-02-29"
+      });
 
 
 let bookingNow = document.querySelector(".bookingNow");
@@ -134,3 +268,4 @@ axios.get(`${_url}/rooms/53`).then(function(response){
     let luxRoomPriceClaim = response.data.price;
     luxRoomPrice.innerHTML = `${luxRoomPriceClaim}元 / 晚`
 });
+
