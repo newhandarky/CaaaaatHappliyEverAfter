b72c4823231a8af870c8b8f87599f161819541f4
newhandarky <newhandarky@gmail.com>
2023-11-25 09:37:14 +0800

    修正錯誤路徑

--- js/admin_bookingList.js
+++ js/admin_bookingList.js
@@ -144,7 +144,7 @@ export function renderTable(arr) {
     let str = "";
     arr.forEach(function (item) {       // 
         str += `<tr>
-                <th class="text-nowrap border-0 text-center d-flex align-items-center justify-content-center" scope="row"><a class="bookingNum adminLink" data-bookingnum="${item.id}" data-userid="${item.userId}" href="../pages/admin_updateBooking.html">${item.id}</a></th>       
+                <th class="text-nowrap border-0 text-center d-flex align-items-center justify-content-center" scope="row"><a class="bookingNum adminLink" data-bookingnum="${item.id}" data-userid="${item.userId}" href="admin_updateBooking.html">${item.id}</a></th>       
                 <td class="text-nowrap text-center">${item.name}</td>
                 <td class="text-nowrap text-center">${item.checkIn}</td>
                 <td class="text-nowrap text-center">${item.quantity}</td>
