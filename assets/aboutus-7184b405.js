e98054a4ff73a4de49346c6060e326752af7c9e2
newhandarky <newhandarky@gmail.com>
2023-11-25 16:15:14 +0800

    Updates

---
+++ assets/aboutus-7184b405.js
@@ -0,0 +1,1 @@
+import"./config-33b8f4bf.js";import"./navbar-9de94bda.js";const t=document.getElementById("aboutus");t.style.color="#FA863E";
--- assets/aboutus-7acb5d57.js
+++
@@ -1,1 +0,0 @@
-import"./config-7dd8b0d7.js";import"./navbar-bcede514.js";const t=document.getElementById("aboutus");t.style.color="#FA863E";
