b1d86f285f9080f31f8fb3bd6ab33294223ab02e
newhandarky <newhandarky@gmail.com>
2023-11-25 09:29:12 +0800

    Updates

---
+++ assets/aboutus-7acb5d57.js
@@ -0,0 +1,1 @@
+import"./config-7dd8b0d7.js";import"./navbar-bcede514.js";const t=document.getElementById("aboutus");t.style.color="#FA863E";
---
+++ assets/importantNotice-5df7c4bc.js
@@ -0,0 +1,1 @@
+const t=document.querySelectorAll("#importantNotice");t.forEach(o=>{o.classList.add("onThisPage")});
