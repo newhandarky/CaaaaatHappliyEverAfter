c544e5781ae6516b202a4d1614765f6b5e57e151
Leo <leo80103@gmail.com>
2023-11-27 18:45:05 +0800

    動畫插件測試成功

---
+++ js/rooms.js
@@ -0,0 +1,23 @@
+let catMotion_01 = bodymovin.loadAnimation({
+  container: document.getElementById("catMotion_01"),
+  render: "svg",
+  loop: true,
+  autoplay: true,
+  path: "../js/catMotion_01.json",
+});
+
+let catMotion_02 = bodymovin.loadAnimation({
+  container: document.getElementById("catMotion_02"),
+  render: "svg",
+  loop: true,
+  autoplay: true,
+  path: "../js/catMotion_02.json",
+});
+
+let catMotion_03 = bodymovin.loadAnimation({
+  container: document.getElementById("catMotion_03"),
+  render: "svg",
+  loop: true,
+  autoplay: true,
+  path: "../js/catMotion_03.json",
+});
