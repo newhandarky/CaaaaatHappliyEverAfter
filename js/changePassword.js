640ed6aae45fd9f195c8b62318d040fea0fea86e
Leo <leo80103@gmail.com>
2023-11-28 14:36:57 +0800

    修改密碼頁面基本架構

--- js/changePassword.js
+++ js/changePassword.js
@@ -4,7 +4,11 @@ import { isLogin } from "./isLogin";
 
 // aside bar 顯示當前頁面
 // 因為有兩個 aside bar 所以要用 querySelectorAll
-const asideLocation = document.querySelectorAll("#importantNotice");
+const asideLocation = document.querySelectorAll("#changePassword");
 asideLocation.forEach((element) => {
   element.classList.add("onThisPage");
 });
+
+//navAside 顯示 因為是會員功能頁面
+const navAside = document.querySelector("#navAside");
+navAside.classList.remove("d-none");
