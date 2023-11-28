94c6c2476f91136d5240aee5ca010c838789c62c
michaelhsia <michaelhsia8716@gmail.com>
2023-11-28 12:08:01 +0800

    完成文章刪除功能

--- js/admin_articleAddNewArticle.js
+++ js/admin_articleAddNewArticle.js
@@ -7,32 +7,77 @@ import tinymce from "tinymce";
 const articleTitle = document.querySelector(".articleTitle");
 const articleId = document.querySelector(".articleId");
 const articleKeyword = document.querySelector(".articleKeyword");
-// const articleContent = document.querySelector("#articleContent");
 const articleAddNewWrap = document.querySelector(".articleAddNewWrap");
+// 儲存草稿按鈕
+const saveAsDraftBtn = document.querySelector(".saveAsDraftBtn");
 
 // 文字編輯器呼叫
 tinymce.init({
   selector: "textarea#articleContent",
 });
 
-// post 新文章到資料庫
+// 發佈新文章功能
 articleAddNewWrap.addEventListener("submit", function (e) {
   e.preventDefault();
 
+  // 取得發佈新文章時間
+  getTime();
+
+  // tinymce 取得內容要使用 getContent()，要放在文字編輯器 init 後面
   let myContent = tinymce.get("articleContent").getContent();
 
-  let obj = {};
-  obj.title = articleTitle.value;
-  obj.content = myContent;
-  obj.keyword = articleKeyword.value;
-  obj.id = parseInt(articleId.value);
+  let obj = {
+    title: articleTitle.value,
+    content: myContent,
+    keyword: articleKeyword.value,
+    lastEditOrPublishTime: localStorage.getItem("articleLastEditOrPublishTime"),
+    status: "已發佈",
+    id: parseInt(articleId.value),
+  };
 
-  axios
-    .post(`${_url}/article`, obj)
-    .then(function (res) {
-      console.log(res.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  // post 到資料庫
+  axios.post(`${_url}/article`, obj).catch(function (error) {
+    console.log(error);
+  });
 });
+
+// 儲存草稿功能
+saveAsDraftBtn.addEventListener("click", function () {
+  // 取得最後編輯時間
+  getTime();
+
+  // tinymce 取得內容要使用 getContent()，要放在文字編輯器 init 後面
+  let myContent = tinymce.get("articleContent").getContent();
+
+  let obj = {
+    title: articleTitle.value,
+    content: myContent,
+    keyword: articleKeyword.value,
+    lastEditOrPublishTime: localStorage.getItem("articleLastEditOrPublishTime"),
+    status: "草稿",
+    id: parseInt(articleId.value),
+  };
+
+  // 儲存草稿的資料存進資料庫
+  axios.post(`${_url}/article`, obj).catch(function (error) {
+    console.log(error);
+  });
+});
+
+// 取得文章發布或最後編輯時間
+function getTime() {
+  // 文章編輯或發佈後，取得當前時間
+  var articleLastEditOrPublishTime = new Date();
+
+  // 將文章編輯或發佈時間轉換為字串，方便存儲，把 toISOString改成 toLocaleString 使用 UTC+8 時區
+  var articleLastEditOrPublishTimeString =
+    articleLastEditOrPublishTime.toLocaleString("en-US", {
+      timeZone: "Asia/Taipei",
+    });
+
+  // 使用 localStorage 存儲文章編輯或發佈時間
+  localStorage.setItem(
+    "articleLastEditOrPublishTime",
+    articleLastEditOrPublishTimeString
+  );
+}
--- js/admin_articleEditArticle.js
+++ js/admin_articleEditArticle.js
@@ -9,14 +9,17 @@ console.log(location.href);
 // https://catroomdb.onrender.com/article?id=601，把id文字跟數字拆開，方便後面 get article/id
 const id = location.href.split("=")[1];
 console.log(id);
+
+// 文章表單監聽用
 const articleEditWrap = document.querySelector(".articleEditWrap");
 const articleTitle = document.querySelector(".articleTitle");
 const articleId = document.querySelector(".articleId");
 const articleKeyword = document.querySelector(".articleKeyword");
 const articleContent = document.querySelector("#articleContent");
-
 // 儲存草稿按鈕
 const saveAsDraftBtn = document.querySelector(".saveAsDraftBtn");
+// 刪除文章按鈕
+const deleteArticleBtn = document.querySelector(".deleteArticleBtn");
 
 // 初始畫面渲染
 function init() {
@@ -44,37 +47,29 @@ init();
 articleEditWrap.addEventListener("submit", function (e) {
   e.preventDefault();
 
+  // 取得發佈時間
+  getTime();
+
   // tinymce 取得內容要使用 getContent()，要放在文字編輯器 init 後面
   let myContent = tinymce.get("articleContent").getContent();
 
+  let obj = {
+    title: articleTitle.value,
+    content: myContent,
+    keyword: articleKeyword.value,
+    lastEditOrPublishTime: localStorage.getItem("articleLastEditOrPublishTime"),
+    status: "已發佈",
+  };
   // patch 會直接動到資料庫
-  axios
-    .patch(`${_url}/article/${id}`, {
-      title: articleTitle.value,
-      keyword: articleKeyword.value,
-      content: myContent,
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+  axios.patch(`${_url}/article/${id}`, obj).catch(function (error) {
+    console.log(error);
+  });
 });
 
 // 儲存草稿功能
 saveAsDraftBtn.addEventListener("click", function (e) {
-  // 文章編輯或發佈後，取得當前時間
-  var articleLastEditOrPublishTime = new Date();
-
-  // 將文章編輯或發佈時間轉換為字串，方便存儲，把 toISOString改成 toLocaleString 使用 UTC+8 時區
-  var articleLastEditOrPublishTimeString =
-    articleLastEditOrPublishTime.toLocaleString("en-US", {
-      timeZone: "Asia/Taipei",
-    });
-
-  // 使用 localStorage 存儲文章編輯或發佈時間
-  localStorage.setItem(
-    "articleLastEditOrPublishTime",
-    articleLastEditOrPublishTimeString
-  );
+  // 取得最後編輯時間
+  getTime();
 
   // tinymce 取得內容要使用 getContent()，要放在文字編輯器 init 後面
   let myContent = tinymce.get("articleContent").getContent();
@@ -92,3 +87,28 @@ saveAsDraftBtn.addEventListener("click", function (e) {
     console.log(error);
   });
 });
+
+// 刪除文章功能
+deleteArticleBtn.addEventListener("click", function () {
+  axios.delete(`${_url}/article/${id}`).catch(function (error) {
+    console.log(error);
+  });
+});
+
+// 取得文章發布或最後編輯時間
+function getTime() {
+  // 文章編輯或發佈後，取得當前時間
+  var articleLastEditOrPublishTime = new Date();
+
+  // 將文章編輯或發佈時間轉換為字串，方便存儲，把 toISOString改成 toLocaleString 使用 UTC+8 時區
+  var articleLastEditOrPublishTimeString =
+    articleLastEditOrPublishTime.toLocaleString("en-US", {
+      timeZone: "Asia/Taipei",
+    });
+
+  // 使用 localStorage 存儲文章編輯或發佈時間
+  localStorage.setItem(
+    "articleLastEditOrPublishTime",
+    articleLastEditOrPublishTimeString
+  );
+}
