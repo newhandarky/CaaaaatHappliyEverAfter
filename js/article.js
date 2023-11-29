48d98e3b5513dc1b36a93bc74d85b6dacf8ede55
Flora Kuo In The House <tingyu1112@gmail.com>
2023-11-30 03:33:00 +0800

    調整顧客回饋、修改貓咪文章呈現方式與關鍵字邏輯、修正送出到資料庫的價錢（刪除元

--- js/article.js
+++ js/article.js
@@ -27,7 +27,7 @@ axios.get(`${_url}/faqs`).then(function(response){
         console.log(item.title);
         console.log(item.content);
         str += `<li class="mb-3  id="">
-        <p class="">
+        <p class="articleTag">
            <a class="border border-primary w-100  btn articleTitle text-start" data-bs-toggle="collapse" href="#${item.title}" role="button" aria-expanded="false" aria-controls="collapseExample">
             ${item.title}
            </a>
@@ -54,18 +54,25 @@ console.log(selector);
 
 axios.get(`${_url}/article`).then(function(response){
     let data = response.data;
-    console.log(data);
-    render(data);
+    let newData = [];
+    data.forEach(function(item){
+        if(item.status == "已發佈"){
+            newData.push(item);
+        }
+    })
+   // console.log(data);
+   render(newData);
+
 
     selector.addEventListener('change', function(e){
         let filterData = [];
 //        console.log(selector.value);
 
         if(selector.value == "全部"){
-            return render(data);
+            return render(newData);
         };
         
-        data.forEach(function(item){
+        newData.forEach(function(item){
             if(item.keyword === selector.value){
                 filterData.push(item);
             };
@@ -78,26 +85,54 @@ axios.get(`${_url}/article`).then(function(response){
      })
 });
 
+//選擇的關鍵字
+axios.get(`${_url}/article`).then(function(response){
+    let keywordList = [];
+    let data = response.data;
+    data.forEach(function(item){
+        keywordList.push(item.keyword)
+    });
+  //  console.log(keywordList);
+
+    let str = `<option selected disabled>關鍵字</option>
+    <option value="全部">全部</option>`;
+    keywordList.forEach(function(item){
+        str += `<option value="${item}">${item}</option>`
+    });
+   // console.log(str);
+    selector.innerHTML = str
+})
 
 
-
+//渲染文章的modal
 function render(data){
     let str = ""
     data.forEach(function(item){
-        str += `<li class="mb-3 " id="">
-        <p class="">
-           <a class="border border-primary w-100   btn articleTitle text-start" data-bs-toggle="collapse" href="#${item.title}" role="button" aria-expanded="false" aria-controls="collapseExample">
-            ${item.title}
-            <span style="color: #e2c6c4;" class="keyword" >#${item.keyword}</span>
-           </a>
+        str += 
+`<li class="border-bottom py-3 border-primary d-flex justify-content-between align-items-center">
+        <!-- Button trigger modal -->
+    <p class="fs-5 articleTag" type="button"  data-bs-toggle="modal" data-bs-target="#${item.title}">
+    ${item.title} <span class="text-primary"> / ${item.keyword}</span>
+    </p>
+    <p>
+    ${item.lastEditOrPublishTime}
+    </p>
 
-        </p>
-         <div class="collapse w-100 " id="${item.title}">
-          <div class="card card-body border-0 mt-1 " style="background-color:#E2c6c4;">
-             ${item.content}
-         </div>
-         </div>
-       </li>`
+<!-- Modal -->
+<div class="modal fade " id="${item.title}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal-dialog modal-dialog-scrollable" style="width:70%">
+<div class="modal-content">
+<div class="modal-header">
+<h5 class="modal-title" id="exampleModalLabel">${item.title}</h5>
+<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+</div>
+<div class="modal-body">
+<p class="fs-5">${item.content}</p>
+</div>
+</div>
+</div>
+</div>
+    </li>`
     });
     console.log(str);
     catArticleList.innerHTML = str;
@@ -140,7 +175,7 @@ var swiper = new Swiper(".mySwiper", {
           if(item.feedback !== ""){
            let userId = item.userId//找到userID
            obj['userId'] = item.userId;
-           obj['feedback']=item.feedback;
+           obj['feedback']=item.feedback.comment;
            userFeedbackList.push(obj)
           };
           
@@ -174,43 +209,61 @@ var swiper = new Swiper(".mySwiper", {
                 console.log(response.data);
                 let feedbackStr = '';
                 let bookingData = response.data;
+                let stars = 0;
+                
                 bookingData.forEach(function(item){
                     if(item.feedback !== ""){
-                        feedbackStr += item.feedback;
-
+                        feedbackStr += item.feedback.comment;
+                        stars += item.feedback.stars;
                     }
                 });
                 console.log(feedbackStr);
+                console.log(stars);
+                console.log(5-stars);
+                let starsSpan = `<span class="material-symbols-outlined">kid_star</span>`
+                let noStarSpan = `<span class="material-symbols-outlined text-light">kid_star</span>`
+
+               
+
+
+
+
+            
+            
 
                 axios.get(`${_url}/cats?userId=${item.userId}`).then(function(response){
-                    console.log(response.data);
+                   // console.log(response.data);
                     let catData = response.data;
                     let catsName = [];
                     catData.forEach(function(item){
-                        console.log(item);
+                      //  console.log(item);
                         catsName.push(item.catName)
                     });
-                    console.log(catsName);
+                   // console.log(catsName);
+                   
 
-                    str += `<li class="swiper-slide d-flex flex-column justify-content-around bg-primary02 border border-light
+                    str += 
+            `<li class="swiper-slide d-flex flex-column justify-content-around bg-primary02 border border-light
                     pb-3 mb-5" style="border-radius: 32px;">
                     <img src="${userPhoto}" alt="" class="rounded-circle" >
 
                     
 
-                    <ul class="d-flex flex-column align-items-start" style="padding-left:32px; padding-right:32px">
+                    <ul class="d-flex flex-column " style="padding-left:32px; padding-right:32px">
                     
-                    <li class="text-start">${area}<span class="text-primary">${catsName}</span> の ${gender}：</li>
-                    <li class="d-flex ">
-                    <span class="material-symbols-outlined me-2">pets</span>
-                    <p>  
-                    ${feedbackStr}</p></li>
+                    <li class="text-start">${area}<span class="text-primary">${catsName}</span> の ${gender}：
+                     ${feedbackStr}
+                    </li>
+                    
+                    <li class="d-flex justify-content-center mt-5">
+                       ${starsSpan.repeat(stars)}${noStarSpan.repeat(5-stars)} 
+                    </li>
                     </ul>
                    
                     
                     
                     
-                </li>`;
+            </li>`;
     
                 swiperWapper.innerHTML = str;
                 feedbackArea.innerHTML=str;
