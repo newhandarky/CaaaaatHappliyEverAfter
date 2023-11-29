5c2dd3297e806d64b4fdecde898f14dad8d080db
newhandarky <newhandarky@gmail.com>
2023-11-29 15:22:48 +0800

    Updates

--- assets/catData-8f2432e7.js
+++ assets/catData-8f2432e7.js
@@ -1,4 +1,4 @@
-import{a as y,_ as $}from"./config-3a988531.js";import{i as g}from"./navbar-64ba0015.js";import"./aside-136a93d9.js";const S=document.querySelectorAll("#catData");S.forEach(s=>{s.classList.add("onThisPage")});const w=document.querySelector("#navAside");w.classList.remove("d-none");const C=JSON.parse(localStorage.getItem("userTokenAndData")),{accessToken:D,user:_}=C,I=document.getElementById("catAdd");function k(){const s=_.id;y.get(`${$}/600/cats?_expand=user&userId=${s}`,{headers:{authorization:`Bearer ${D}`}}).then(a=>{const l=a.data,i=JSON.parse(localStorage.getItem("userTokenAndData"));i.showCatinCatPage=l,localStorage.setItem("userTokenAndData",JSON.stringify(i));let n=[];l.forEach((e,t)=>{console.log(e);const{catName:c,catPhoto:o,gender:d,birthday:f,catBreeds:h,colors:v,weight:p,id:u}=e;console.log(c,d,f,h,v,p,u);const A=()=>d==="male"?"男 ":d==="female"?"女 ":d==="other"?"其他 ":"未定義",b=document.getElementById("catInfo");let E=JSON.stringify(`
+import{a as D,_ as $}from"./config-3a988531.js";import{i as m}from"./navbar-64ba0015.js";import"./aside-136a93d9.js";const w=document.querySelectorAll("#catData");w.forEach(s=>{s.classList.add("onThisPage")});const C=document.querySelector("#navAside");C.classList.remove("d-none");const _=JSON.parse(localStorage.getItem("userTokenAndData")),{accessToken:I,user:k}=_,A=document.getElementById("catAdd");function B(){const s=k.id;D.get(`${$}/600/cats?_expand=user&userId=${s}`,{headers:{authorization:`Bearer ${I}`}}).then(a=>{document.querySelector("#loading").classList.add("d-none");const u=a.data,n=JSON.parse(localStorage.getItem("userTokenAndData"));n.showCatinCatPage=u,localStorage.setItem("userTokenAndData",JSON.stringify(n));let l=[];u.forEach((e,t)=>{console.log(e);const{catName:c,catPhoto:o,gender:r,birthday:h,catBreeds:v,colors:p,weight:y,id:g}=e;console.log(c,r,h,v,p,y,g);const b=()=>r==="male"?"男 ":r==="female"?"女 ":r==="other"?"其他 ":"未定義",E=document.getElementById("catInfo");let S=JSON.stringify(`
         <div class="d-flex flex-column">
             <div
               class="catData d-flex flex-lg-row flex-column justify-content-center align-items-center"
@@ -14,19 +14,19 @@ import{a as y,_ as $}from"./config-3a988531.js";import{i as g}from"./navbar-64ba
                   <h1 id="catName_${t}" class="catName">${c}</h1>
                   <ul>
                     <li id="catGender_${t}" class="catGender">
-                      性別：${A()}
+                      性別：${b()}
                     </li>
                     <li id="catBirthday_${t}" class="catBirthday">
-                      生日：${f}
+                      生日：${h}
                     </li>
                     <li id="catBreeds_${t}" class="catBreeds">
-                      品種：${h}
+                      品種：${v}
                     </li>
                     <li id="catColors_${t}" class="catColors">
-                      花色：${v}
+                      花色：${p}
                     </li>
                     <li id="catWeight_${t}" class="catWeight">
-                      體重：${p} Kg
+                      體重：${y} Kg
                     </li>
                   </ul>
                 </div>
@@ -57,7 +57,7 @@ import{a as y,_ as $}from"./config-3a988531.js";import{i as g}from"./navbar-64ba
                 <a
                   id="catEdit_${t}"
                   data-Index="${t}"
-                  data-catId="${u}"
+                  data-catId="${g}"
                   class="catEdit"
                   href="./editCat.html"
                   >編輯貓咪</a
@@ -67,7 +67,7 @@ import{a as y,_ as $}from"./config-3a988531.js";import{i as g}from"./navbar-64ba
                 <button
                   id="catDelete_${t}"
                   data-Index="${t}"
-                  data-catId="${u}"
+                  data-catId="${g}"
                   class="catDelete"
                 >
                   刪除貓咪
@@ -76,8 +76,8 @@ import{a as y,_ as $}from"./config-3a988531.js";import{i as g}from"./navbar-64ba
             </div>
           </div>
           <hr class="m-5" style="border: 1.5px solid black;">
-        `);n+=JSON.parse(E),console.log(n),b.innerHTML=n});const r=document.querySelectorAll(".catEdit");console.log(r),r.forEach(e=>{e.addEventListener("click",t=>{t.preventDefault();const c=e.getAttribute("href"),o=e.getAttribute("data-catId");console.log(o),localStorage.setItem("catEditId",JSON.stringify(o)),g(c)})});const m=document.querySelectorAll(".catDelete");console.log(m),m.forEach(e=>{e.addEventListener("click",t=>{t.preventDefault(),g();const c=e.getAttribute("data-catId");console.log(c),y.delete(`${$}/600/cats/${c}`,{headers:{authorization:`Bearer ${D}`}}).then(o=>{console.log(o),alert("刪除貓咪成功"),window.location.href="./catData.html"}).catch(o=>{console.log(o),alert("刪除貓咪失敗"),window.location.href="./login.html"})})})}).catch(a=>{console.log(a);const l=a.response.data;if(console.log(l),l==="Private resource access: entity must have a reference to the owner id"){const i=document.getElementById("catInfo");let n=JSON.stringify(`
+        `);l+=JSON.parse(S),console.log(l),E.innerHTML=l});const i=document.querySelectorAll(".catEdit");console.log(i),i.forEach(e=>{e.addEventListener("click",t=>{t.preventDefault();const c=e.getAttribute("href"),o=e.getAttribute("data-catId");console.log(o),localStorage.setItem("catEditId",JSON.stringify(o)),m(c)})});const f=document.querySelectorAll(".catDelete");console.log(f),f.forEach(e=>{e.addEventListener("click",t=>{t.preventDefault(),m();const c=e.getAttribute("data-catId");console.log(c),D.delete(`${$}/600/cats/${c}`,{headers:{authorization:`Bearer ${I}`}}).then(o=>{console.log(o),alert("刪除貓咪成功"),window.location.href="./catData.html"}).catch(o=>{console.log(o),alert("刪除貓咪失敗"),window.location.href="./login.html"})})})}).catch(a=>{console.log(a);const d=a.response.data;if(console.log(d),d==="Private resource access: entity must have a reference to the owner id"){const n=document.getElementById("catInfo");let l=JSON.stringify(`
         <div id="catContainer_add" class="catContainer d-flex justify-content-center align-items-center m-5">
           <h1 id="catAddTitle" class="catAddTitle">快來新增您的愛貓！</h1>        
         </div>
-      `),r=JSON.parse(n);i.innerHTML=r}else alert(a),window.location.href="./login.html"})}k();I.addEventListener("click",s=>{s.preventDefault();const a=I.getAttribute("href");console.log(a),g(a)});
+      `),i=JSON.parse(l);n.innerHTML=i}else alert(a),window.location.href="./login.html";document.querySelector("#loading").classList.add("d-none")})}B();A.addEventListener("click",s=>{s.preventDefault();const a=A.getAttribute("href");console.log(a),m(a)});
